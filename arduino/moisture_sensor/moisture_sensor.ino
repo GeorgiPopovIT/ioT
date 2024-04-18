d58e7e1fd564d323e8c688e33465c3ecb6dcbcf7
GeorgiPopovIT <gogopopo26062003@gmail.com>
2024-04-18 21:28:28 +0300

    Install mosquitto and eclipse-paho libraries form mqtt comunication.

--- arduino/moisture_sensor/moisture_sensor.ino
+++ arduino/moisture_sensor/moisture_sensor.ino
@@ -1,9 +1,21 @@
+#define SECRET_SSID "Popovi"
+#define SECRET_PASS "1234"
 // define led according to pin diagram
 int led1 = D10;
 int led2 = D9;
 int led3 = D8;
 
+char ssid[] = SECRET_SSID;        // your network SSID (name)
+char pass[] = SECRET_PASS;    // your network password (use for WPA, or use as key for WEP)
 
+WiFiClient wifiClient;
+MqttClient mqttClient(wifiClient);
+
+const char broker[] = "test.mosquitto.org";
+int port = 1883;
+const char topic[]  = "real_unique_topic";
+const char topic2[]  = "real_unique_topic_2";
+const char topic3[]  = "real_unique_topic_3";
 
 void setup() {
 
@@ -43,7 +55,7 @@ int calculatePercentage(int start, int end, int current) {
   }
 }
 
-void printSoilMoisture(uint8_t id, uint8_t pin) {
+int printSoilMoisture(uint8_t id, uint8_t pin) {
   uint32_t soil = analogReadMilliVolts(pin);
   int percentage = calculatePercentage(2250, 1050, soil);
   Serial.print("Soil moisture ");
@@ -52,19 +64,33 @@ void printSoilMoisture(uint8_t id, uint8_t pin) {
   Serial.print(percentage);
   Serial.println("%");
 
+  return percentage;
+
 }
 
 void loop() {
 
-  printSoilMoisture(1,A0);
+  int percentage = printSoilMoisture(1,A0);
+  if(percentage <= 10){
+    digitalWrite(led1, HIGH);
+       digitalWrite(led2, LOW);
+  }
+  else if( percentage > 10 && percentage <= 70){
+   digitalWrite(led2, HIGH);
+   digitalWrite(led1, LOW);
+  }
+  else{
+    digitalWrite(led3, HIGH);
+    digitalWrite(led1, LOW);
+   digitalWrite(led2, LOW);
+
+  }
+delay(500); 
 
 
-  digitalWrite(led1, HIGH);   // turn the LED on
-  digitalWrite(led2, HIGH);
-  digitalWrite(led3, HIGH);
-  delay(500);               // wait for a second
-  digitalWrite(led1, LOW);    // turn the LED off
-  digitalWrite(led2, HIGH);
-  digitalWrite(led3, LOW);
-  delay(500);    
+  mqttClient.poll();
+
+  mqttClient.beginMessage(percentage);
+  mqttClient.print(Rvalue);
+  mqttClient.endMessage();
 }
